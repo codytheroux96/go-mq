1128bd62b7456ac25714896e6f8614301f545ce8
codytheroux96 <cody.theroux3@gmail.com>
2025-04-22 19:32:29 -0400

    repository interface for in memory db

--- internal/repository/memory.go
+++ internal/repository/memory.go
@@ -1,2 +1,9 @@
 package repository
 
+type Memory struct {
+
+}
+
+func New() *Memory {
+	return &Memory{}
+}
---
+++ internal/repository/repository.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"github.com/codytheroux96/go-mq/internal/core"
+)
+
+type Repository interface {
+	CreateTopic(name string) error
+	Publish(topic string, msg *core.Message) error
+	Fetch(topic, consumerID string, limit int) ([]*core.Message, error)
+	CommitOffset(topic, consumerID string, offset int) error
+	GetOffset(topic, consumerID string) (int, error)
+}
