7f90c4ffbe1f25eba155fdee2c505cda5db3627a
codytheroux96 <cody.theroux3@gmail.com>
2025-04-22 20:54:26 -0400

    setting up implementation for my interface inside memory.go

--- internal/repository/memory.go
+++ internal/repository/memory.go
@@ -1,9 +1,43 @@
 package repository
 
+import (
+	"sync"
+
+	"github.com/codytheroux96/go-mq/internal/core"
+)
+
 type InMemoryRepo struct {
+	topics map[string]*topicEntry
+	mu     sync.RWMutex
+}
+
+type topicEntry struct {
+	messages []*core.Message
+	offsets  map[string]int // consumerID -> offset
+}
+
+func NewInMemoryRepo() *InMemoryRepo {
+	return &InMemoryRepo{
+		topics: make(map[string]*topicEntry),
+	}
+}
+
+func (m *InMemoryRepo) CreateTopic(name string) error {
+	
+}
+
+func (m *InMemoryRepo) Publish(topic string, msg *core.Message) error {
 
 }
 
-func New() *InMemoryRepo {
-	return &InMemoryRepo{}
+func (m *InMemoryRepo) Fetch(topic, consumerID string, limit int) ([]*core.Message, error) {
+
+}
+
+func (m *InMemoryRepo) CommitOffset(topic, consumerID string, offset int) error {
+
+}
+
+func (m *InMemoryRepo) GetOffset(topic, consumerID string) (int, error) {
+
 }
