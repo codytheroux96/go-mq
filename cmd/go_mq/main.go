8bba9af899499dc1dbc02331b73c6239967fc01a
codytheroux96 <cody.theroux3@gmail.com>
2025-04-29 20:31:03 -0400

    added a little pub/sub live test in main.go

--- cmd/go_mq/main.go
+++ cmd/go_mq/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/codytheroux96/go-mq/internal/core"
 	"github.com/codytheroux96/go-mq/internal/repository"
 )
 
 func main() {
+	// ---- Fetch/Commit Offset Flow Test ----
+	fmt.Println("\n--- Live Fetch/Commit Offset Flow Test ---")
+
 	repo := repository.NewInMemoryRepo()
 
 	err := repo.CreateTopic("test-topic")
@@ -45,4 +49,29 @@ func main() {
 	for _, msg := range messages {
 		fmt.Println(string(msg.Body))
 	}
+
+	// ---- Pub/Sub Test ----
+	fmt.Println("\n--- Live Subscriber Test ---")
+
+	inbox, err := repo.Subscribe("test-topic", "consumer-2")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for msg := range inbox {
+			fmt.Printf("[consumer-2] Received: %s\n", string(msg.Body))
+		}
+	}()
+
+	for i := 6; i <= 10; i++ {
+		msg := &core.Message{
+			Body: []byte(fmt.Sprintf("Live Message %d", i)),
+		}
+		if err := repo.Publish("test-topic", msg); err != nil {
+			panic(err)
+		}
+	}
+
+	time.Sleep(200 * time.Millisecond)
 }
